6f5264851357b6d0f8a331dc3c8658ea0028ffab
Alejandra <alejandrariscanevo@gmail.com>
2021-02-13 17:07:39 -0500

    v1

--- starter-code/server/app.js
+++ starter-code/server/app.js
@@ -52,5 +52,16 @@ app.locals.title = 'Express - Generated with IronGenerator';
 const index = require('./routes/index');
 app.use('/', index);
 
+const mongoose = require('mongoose')
+
+const PostSchema = new mongoose.Schema({
+    title: String,
+    contents: Array,
+    image: String,
+    releaseDate: Date,
+    special: boolean
+})
+
+module.exports = mongoose.model('Post', PostSchema)
 
 module.exports = app;
